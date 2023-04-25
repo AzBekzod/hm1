2771e0fdd9d689c32bed6bb3a6efc3119504aecd
Bekzod <bek2k00@mail.ru>
2023-04-25 23:44:54 +0500

    add changes

--- homework3.py
+++ homework3.py
@@ -1,7 +1,2 @@
-def solve(n):
-    n1 = n
-    nn = int(str(n) * 2)
-    nnn = int(str(n) * 3)
-    print(n1 + nn + nnn)
-
-    solve(10)
+n = input("Введите число: ")
+print(f'Результат: {int(n) + int(n*2) + int(n*3)} ')
